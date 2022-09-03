a10a64ad88ed4142300378782c02e1050dcb525e
checkiamsiam <issiam02415@gmail.com>
2022-09-03 20:18:23 +0600

    4 api created

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -34,9 +34,32 @@ module.exports.addUser = (req, res, next) => {
       next(err);
     } else {
       const parsedData = JSON.parse(data);
-      const newData = req.body;
-      newData.id = parsedData.length + 1;
-      parsedData.push(newData);
+      if (req.body.gender && req.body.name && req.body.contact && req.body.adress && req.body.photoUrl) {
+        const newData = req.body;
+        newData.id = parsedData.length + 1;
+        parsedData.push(newData);
+        fs.writeFile("./public/users.json", JSON.stringify(parsedData), (err) => {
+          if (err) {
+            next(err);
+          } else {
+            res.status(200).send({ success: true, message: "user added successfully" });
+          }
+        });
+      } else {
+        res.send({ success: false, message: "please provide all necessary data" });
+      }
+    }
+  });
+};
+module.exports.updateUser = (req, res, next) => {
+  fs.readFile(`./public/users.json`, (err, data) => {
+    if (err) {
+      next(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      const updatedDoc = parsedData.find((d) => d.id === req.body.id);
+      const index = parsedData.indexOf(updatedDoc);
+      parsedData[index] = req.body;
       fs.writeFile("./public/users.json", JSON.stringify(parsedData), (err) => {
         if (err) {
           next(err);
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getRandomUser, getAll, addUser } = require('../controllers/user.controllers');
+const { getRandomUser, getAll, addUser, updateUser } = require('../controllers/user.controllers');
 const userRoute = express.Router()
 
 userRoute.get('/random' , getRandomUser)
 userRoute.get('/all' , getAll)
 userRoute.post('/save' , addUser)
+userRoute.patch('/update' , updateUser)
 
 module.exports = userRoute ;
